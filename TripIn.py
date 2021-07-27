4d08756c44607d16f29e290bef658457373272c1
Shinchan1511 <abhishekpathakdidwana@gmail.com>
2021-07-27 18:09:08 +0530

    image upload feature added

--- TripIn.py
+++ TripIn.py
@@ -1,18 +1,22 @@
-from flask import Flask, jsonify, request, make_response, redirect, url_for
+from flask import Flask, jsonify, request, make_response, redirect, url_for, send_from_directory, abort
 import pymongo
-import bcrypt, jwt, datetime, uuid
+import bcrypt, jwt, datetime, uuid, os
 from functools import wraps
-import gridfs, codecs
+from werkzeug.utils import secure_filename
+
+
+UPLOAD_FOLDER = 'E:\Study\Final_project\images'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'secretkeylol'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] =  2 * 1024 * 1024
 
 myClient = pymongo.MongoClient("mongodb+srv://shinchan:cvcvpo123@mycluster1.fzgzf.mongodb.net/tripin?retryWrites=true&w=majority",ssl=True,ssl_cert_reqs='CERT_NONE')
 mydb = myClient["tripin"]
 users = mydb["user_data"]
-gf = gridfs.GridFS(mydb)
-#user_login = mydb["user_login"]
 
 # ---------------------------------------------------///////       User routes      ///////////-------------------------------------------------
 
@@ -25,8 +29,6 @@ def token_verify(f):
         if 'x-access-token' in request.headers:
             token = request.headers['x-access-token']
         
-        # token = request.args.get('token')
-        #print(token)
 
         if not token:
             return jsonify({
@@ -41,6 +43,10 @@ def token_verify(f):
         return f(current_user, *args, **kwargs)
 
     return decorated
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/register', methods = ['POST'])
 def register():
@@ -70,24 +76,21 @@ def register():
 
 @app.route('/login', methods = ['POST'])
 def login():
-    #qry = ({"username": request.args["username"]})
     login_name = users.find_one({"username": request.args["username"]})
     
     if login_name:
         if bcrypt.checkpw(request.args['password'].encode('utf-8'), login_name['password']):
             token = jwt.encode({'username': login_name['username'], 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours = 24)}, app.config['SECRET_KEY'], algorithm="HS256") 
-            # dashboard(login_name)
-            #return dashboard(login_name['username'])
-            # return jsonify({
-            #     'message': 'login successful',
-            #     'token': token
-            # })
-            return make_response(jsonify({'token' : token}), 201)
+            
+            return make_response(jsonify({
+                'message': "Login Successfull",
+                'token' : token
+                }), 201)
         else:
-            return jsonify({'message':"user does not match"})
+            return jsonify({'message':"username or password does not match"})
 
     else:
-        return make_response("user not verify", 401)
+        return make_response("user not exists", 401)
 
 
 @app.route('/me', methods = ['GET'])
@@ -95,45 +98,45 @@ def login():
 def me(current_user):
     user = users.find_one({"username": current_user['username']})
 
-    #img = user['image_data']
-
-    if not 'image_data' in user:
-        image = "no_image.jpg"
-    else:
-        img = user['image_data']
-        image = gf.get(img['_id'])
-        base64_data = codecs.encode(image.read(), 'base64')
-        image = base64_data.decode('utf-8')
-
     me_data = {
         "_id": user['_id'],
         "username": user['username'],
         "name": user['name'],
         "email": user['email'],
         "mobile_no": user['mobile_no'],
-        "image":image
     }
 
     return jsonify(me_data)
 
-@app.route('/image', methods=["PUT"])
+@app.route('/images', methods=['GET', 'POST'])
 @token_verify
-def saveImage(current_user):
+def upload_file(current_user):
     user = users.find_one({"username": current_user['username']})
-    myQuery = {"username": user['username']}
-    #if 'image' in request.files:
-    image = request.files['image']
-    pic_name = request.form.get('pic_name')
-    id = gf.put(image, filename=pic_name)
-    query = {"$set":{
-        "image_data":{
-            '_id': id,
-            'pic_name': pic_name,
-    }}}
-    status = users.update_one(myQuery, query)
-    if status:
-        return jsonify({'result': 'Image uploaded successfully'}), 201
-    return jsonify({'result': 'Error occurred during uploading'}), 500
+
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return jsonify({"message":'no file part'})
+
+        file = request.files['file']
+
+        if file.filename == '':
+            return jsonify({"message":'no file selected'})
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            #file.save(os.path.join(app.config['UPLOAD_FOLDER'], user['_id']) + '.' + filename.rsplit('.', 1)[1].lower())
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], user['_id']) + '.jpg')
+            return jsonify({"message":'file uploaded successfully'})
+
+    elif request.method == 'GET':
+
+        img = user['_id']
+        img = f"{img}.jpg"
+        #return str(img)
+        try:
+            return send_from_directory(app.config["UPLOAD_FOLDER"], filename=img, as_attachment=True)
+        except FileNotFoundError:
+            abort(404)
 
 @app.route('/me', methods = ['PUT'])
 @token_verify
@@ -169,8 +172,6 @@ def delete_user(current_user):
 #         return jsonify({'message':"user already exists"})
         
 #     else:
-        
-#         #hashpass = bcrypt.hashpw(request.args['password'].encode('utf-8'), bcrypt.gensalt(10))
 #         salt = bcrypt.gensalt(10)
 #         hashpass =  bcrypt.hashpw(request.args['password'].encode('utf-8'),salt)
 
@@ -181,7 +182,6 @@ def delete_user(current_user):
 #             "name": request.args["name"],
 #             "email": request.args["email"],
 #             "mobile_no": request.args["mobile_no"],
-#             #"image_data": "profimg.jpg",
 #             })
 #         return jsonify({'message':"user registered successfully"})
 
